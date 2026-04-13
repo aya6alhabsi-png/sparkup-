edb8d568b62e157ba542a2a1431687211348cc45
Alanood Alsadi <85736439+alanoodsaid@users.noreply.github.com>
2026-04-13 19:52:07 +0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { AppProvider } from "./context/AppContext";
+
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -11,29 +13,54 @@ import Contact from "./components/Contact";
 import InnovatorPage from "./components/InnovatorPage";
 import FunderPage from "./components/FunderPage";
 import ShowEvents from "./components/ShowEvents";
+import EventsPage from "./components/EventsPage";
+import IdeasBoard from "./components/IdeasBoard";
+import FeedbackPage from "./components/FeedbackPage";
+import ReportsPage from "./components/ReportsPage";
+import FundingPage from "./components/FundingPage";
+import CertificatesPage from "./components/CertificatesPage";
+import NotificationsPage from "./components/NotificationsPage";
+import UserManagementPage from "./components/UserManagementPage";
+import ReviewerRegister from "./components/ReviewerRegister";
+import ReviewerPage from "./components/ReviewerPage";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./theme.css";
 
 function App() {
   return (
-    <Routes>
-    
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgetpass" element={<ForgetPass />} />
-      <Route path="/resetpass" element={<ResetPass />} />
-      <Route path="/adminlogin" element={<Adminlogin />} />
-      <Route path="/admin" element={<AdminPage />} />
+    <AppProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgetpass" element={<ForgetPass />} />
+        <Route path="/resetpass" element={<ResetPass />} />
+        <Route path="/adminlogin" element={<Adminlogin />} />
+        <Route path="/admin" element={<AdminPage />} />
 
-      <Route path="/innovator" element={<InnovatorPage />} />
-      <Route path="/funder" element={<FunderPage />} />
+        <Route path="/reviewer-register" element={<ReviewerRegister />} />
+        <Route path="/reviewer" element={<ReviewerPage />} />
 
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/events" element={<ShowEvents />} />
-      <Route path="*" element={<Home />} />
-    </Routes>
+        <Route path="/innovator" element={<InnovatorPage />} />
+        <Route path="/funder" element={<FunderPage />} />
+
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/events-ui" element={<ShowEvents />} />
+
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/ideas" element={<IdeasBoard />} />
+        <Route path="/feedback" element={<FeedbackPage />} />
+        <Route path="/reports" element={<ReportsPage />} />
+        <Route path="/funding" element={<FundingPage />} />
+        <Route path="/certificates" element={<CertificatesPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/admin/users" element={<UserManagementPage />} />
+
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </AppProvider>
   );
 }
 
